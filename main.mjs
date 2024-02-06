1a5457d3ffaf7b3be346165bd89535450f38e0fe
Somē Cho <some.cho@kimweb.de>
2024-02-06 08:22:22 +0100

    puppeteer example

--- main.mjs
+++ main.mjs
@@ -1,12 +1,41 @@
+//@ts-check
 import { remote } from "webdriverio";
 
 let browser = await remote({
   capabilities: {
     browserName: "chrome",
   },
-  services: ["devtools"],
 });
 
-await browser.cdp("Network", "enable");
-await browser.url("http://google.com");
-await browser.debug();
+await browser.url("https://www-test.ifolor.de");
+
+// const logs = await browser.getLogs("browser");
+// console.log(logs);
+
+const ppt = await browser.getPuppeteer();
+
+await browser.call(async () => {
+  const pages = await ppt.pages();
+  pages[0].on("console", (msg) => {
+    console.log(msg.text());
+    console.log(msg.type());
+    console.log(msg.args());
+  });
+});
+
+// const page = await pup.newPage();
+
+// await page.setRequestInterception(true);
+// page.on("request", async (request) => {
+//   console.log("---------------------------------------");
+//   console.log(request.url());
+//   console.log(request.headers());
+//   console.log(request.method());
+//   console.log(request.postData());
+//   console.log("---------------------------------------");
+//   request.continue();
+// });
+// page.on("console", (msg) => console.log(msg.text()));
+// page.goto("https://www-test.ifolor.de");
+
+// await browser.debug();
